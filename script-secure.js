5029f7a9844d4ae355008ff467eb70c567b77dbf
hybritworld <130080899+hybritworld@users.noreply.github.com>
2023-07-16 01:43:36 +0200

    Aktualizacja plików o komentarze i dodanie edycji

--- script-secure.js
+++ script-secure.js
@@ -1,123 +1,191 @@
 const loggedIn = sessionStorage.getItem('loggedIn');
 
+// Sprawdzenie, czy użytkownik jest zalogowany
 if (loggedIn !== 'true') {
-    window.location.href = 'index.html';
+  // Przekierowanie użytkownika na stronę logowania
+  window.location.href = 'index.html';
 }
 
+// Pobranie elementów DOM
 const introElement = document.getElementById('intro');
 const loginPanelElement = document.getElementById('login-panel');
-const loginForm = document.querySelector('#login-form');
 const taskList = document.querySelector('#task-list');
 
+// Ukrycie panelu logowania
 loginPanelElement.style.display = 'none';
 
+// Obsługa zdarzenia animacji
 introElement.addEventListener('animationend', () => {
-    introElement.style.display = 'none';
-    loginPanelElement.style.display = 'inherit';
-    loginPanelElement.classList.add('login-fade-in');
+  introElement.style.display = 'none';
+  loginPanelElement.style.display = 'inherit';
+  loginPanelElement.classList.add('login-fade-in');
 });
 
+// Adres API
 const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
+
+// Pobranie formularza zadań
 const taskForm = document.getElementById('task-form');
 const nameInput = document.getElementById('name');
 
+// Pobranie listy zadań przy ładowaniu strony
+fetchTasks();
+
+// Obsługa zdarzenia dodawania zadania
 taskForm.addEventListener('submit', async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const value = nameInput.value.trim();
+  const value = nameInput.value.trim();
 
-    if (value !== '') {
-        await addTask(value);
-    }
+  if (value !== '') {
+    await addTask(value);
+  }
 });
 
 // Pobranie listy zadań
 async function fetchTasks() {
-    try {
-        const response = await fetch(`${apiUrl}?_limit=5`);
-        const tasks = await response.json();
-        tasks.forEach(task => addTaskToList(task));
-    } catch (error) {
-        console.error('Wystąpił błąd z pobieraniem danych: ', error);
-    }
+  try {
+    const response = await fetch(`${apiUrl}?_limit=5`);
+    const tasks = await response.json();
+
+    clearTaskList();
+    tasks.forEach(task => addTaskToList(task));
+  } catch (error) {
+    console.error('Wystąpił błąd z pobieraniem danych: ', error);
+  }
 }
 
+// Dodanie zadania do listy
 function addTaskToList(task) {
-    const listItem = document.createElement('li');
-    const spanItem = document.createElement('span');
+  const listItem = document.createElement('li');
+  listItem.classList.add('mt-2', 'flex', 'items-center');
 
-    listItem.classList.add('mt-2');
-    spanItem.textContent = task.title;
-    spanItem.classList.add('text-white');
+  const checkboxContainer = document.createElement('label');
+  checkboxContainer.classList.add('flex', 'items-center');
 
-    if (task.completed) {
-        spanItem.classList.add('line-through');
+  const checkbox = document.createElement('input');
+  checkbox.type = 'checkbox';
+  checkbox.checked = task.completed;
+  checkbox.classList.add('form-checkbox', 'mr-2', 'h-5', 'w-5');
+  checkbox.addEventListener('change', async () => {
+    await updateTaskStatus(task.id, checkbox.checked);
+    spanItem.classList.toggle('line-through', checkbox.checked);
+  });
+
+  checkboxContainer.appendChild(checkbox);
+  listItem.appendChild(checkboxContainer);
+
+  const spanItem = document.createElement('span');
+  spanItem.textContent = task.title;
+  spanItem.classList.add('text-white', 'flex-grow');
+  spanItem.classList.toggle('line-through', task.completed);
+
+  listItem.appendChild(spanItem);
+
+  const editButton = createButton('Edytuj', 'bg-blue-500', 'hover:bg-blue-600', 'ml-2', 'py-1', 'px-2', 'rounded');
+  editButton.addEventListener('click', async function () {
+    await editTask(task.id, spanItem);
+  });
+  listItem.appendChild(editButton);
+
+  const deleteButton = createButton('Usuń', 'bg-red-500', 'hover:bg-red-600', 'ml-2', 'py-1', 'px-2', 'rounded');
+  deleteButton.addEventListener('click', async () => {
+    if (confirm('Czy na pewno chcesz usunąć to zadanie?')) {
+      await deleteTask(task.id);
+      listItem.remove();
     }
+  });
+  listItem.appendChild(deleteButton);
 
-    listItem.appendChild(spanItem);
+  taskList.appendChild(listItem);
+}
 
-    const deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Usuń';
-    deleteButton.classList.add('bg-red-500', 'hover:bg-red-600', 'ml-2', 'py-1', 'px-2', 'rounded');
-    deleteButton.addEventListener('click', async () => {
-        await deleteTask(task.id);
-        listItem.remove();
+// Dodanie zadania do listy (zapytanie POST)
+async function addTask(name) {
+  try {
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      body: JSON.stringify({
+        completed: false,
+        title: name,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
     });
 
-    listItem.appendChild(deleteButton);
-    taskList.appendChild(listItem);
+    const newTask = await response.json();
+    addTaskToList(newTask);
+    nameInput.value = '';
+  } catch (error) {
+    console.error('Wystąpił błąd z wysłaniem danych: ', error);
+  }
 }
 
-// dodaj element na listę -> POST
-async function addTask(name) {
-    try {
-        const response = await fetch(apiUrl, {
-            method: 'POST',
-            body: JSON.stringify({
-                completed: false,
-                title: name,
-            }),
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-            },
-        });
-
-        const newTask = await response.json();
-        addTaskToList(newTask);
-        nameInput.value = '';
-    } catch (error) {
-        console.error('Wystąpił błąd z wysłaniem danych: ', error);
-    }
-}
-
-
-// usuwanie elementu z listy -> DELETE
+// Usunięcie zadania z listy (zapytanie DELETE)
 async function deleteTask(id) {
-    try {
-        await fetch(`${apiUrl}/${id}`, {
-            method: 'DELETE',
-        });
-    } catch (error) {
-        console.error('Wystąpił błąd z usunięciem danych: ', error);
-    }
+  try {
+    await fetch(`${apiUrl}/${id}`, {
+      method: 'DELETE',
+    });
+  } catch (error) {
+    console.error('Wystąpił błąd z usunięciem danych: ', error);
+  }
 }
 
-// aktualizowanie zadania -> PATCH
+// Edycja zadania
+async function editTask(id, spanItem) {
+  const newName = prompt('Wprowadź nową nazwę zadania', spanItem.textContent);
+
+  if (newName && newName.trim() !== '') {
+    await updateTask(id, newName);
+    spanItem.textContent = newName;
+  }
+}
+
+// Aktualizacja zadania (zapytanie PATCH)
 async function updateTask(id, name) {
-    try {
-        await fetch(`${apiUrl}/${id}`, {
-            method: 'PATCH',
-            body: JSON.stringify({
-                title: name,
-            }),
-            headers: {
-                'Content-type': 'application/json; charset=UTF-8',
-            },
-        });
-    } catch (error) {
-        console.error('Wystąpił błąd z aktualizacją danych: ', error);
-    }
+  try {
+    await fetch(`${apiUrl}/${id}`, {
+      method: 'PATCH',
+      body: JSON.stringify({
+        title: name,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+  } catch (error) {
+    console.error('Wystąpił błąd z aktualizacją danych: ', error);
+  }
 }
 
+// Aktualizacja stanu zadania (zapytanie PATCH)
+async function updateTaskStatus(id, completed) {
+  try {
+    await fetch(`${apiUrl}/${id}`, {
+      method: 'PATCH',
+      body: JSON.stringify({
+        completed: completed,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
+  } catch (error) {
+    console.error('Wystąpił błąd z aktualizacją danych: ', error);
+  }
+}
 
-fetchTasks();
+// Wyczyszczenie listy zadań
+function clearTaskList() {
+    taskList.innerHTML = ''; // Wyczyszczenie zawartości listy poprzez przypisanie pustego stringa
+  }
+  
+// Tworzenie przycisku
+function createButton(text, ...classNames) {
+const button = document.createElement('button'); // Utworzenie nowego elementu button
+button.textContent = text; // Ustawienie tekstu w przycisku
+button.classList.add(...classNames); // Dodanie klas stylów do przycisku
+return button; // Zwrócenie utworzonego przycisku
+}
--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-const introElement = document.getElementById('intro');
-const loginPanelElement = document.getElementById('login-panel');
-const loginForm = document.querySelector('#login-form');
+const introElement = document.getElementById('intro'); // Pobranie elementu o id 'intro'
+const loginPanelElement = document.getElementById('login-panel'); // Pobranie elementu o id 'login-panel'
+const loginForm = document.querySelector('#login-form'); // Pobranie elementu formularza o id 'login-form'
 
-loginPanelElement.style.display = 'none';
+loginPanelElement.style.display = 'none'; // Ukrycie panelu logowania na początku
 
 introElement.addEventListener('animationend', () => {
-    introElement.style.display = 'none';
-    loginPanelElement.style.display = 'inherit';
-    loginPanelElement.classList.add('login-fade-in');
+  introElement.style.display = 'none'; // Ukrycie elementu intro po zakończeniu animacji
+  loginPanelElement.style.display = 'inherit'; // Wyświetlenie panelu logowania
+  loginPanelElement.classList.add('login-fade-in'); // Dodanie klasy stylu do animacji
 });
 
 loginForm.addEventListener('submit', event => {
-    event.preventDefault();
+  event.preventDefault(); // Zapobieganie domyślnej akcji formularza (przeładowanie strony)
 
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+  const username = document.getElementById('username').value; // Pobranie wartości pola username
+  const password = document.getElementById('password').value; // Pobranie wartości pola password
 
-    if (username === 'sda123' && password === 'sda123') {
-        sessionStorage.setItem('loggedIn', 'true');
-        window.location.href = 'secure-page.html';
-    } else {
-        alert('Nieprawidłowy login lub hasło!');
-    }
+  if (username === 'sda123' && password === 'sda123') {
+    sessionStorage.setItem('loggedIn', 'true'); // Ustawienie flagi zalogowania w sessionStorage
+    window.location.href = 'secure-page.html'; // Przekierowanie użytkownika do strony zabezpieczonej
+  } else {
+    alert('Nieprawidłowy login lub hasło!'); // Wyświetlenie alertu w przypadku nieprawidłowych danych logowania
+  }
 });
